19c896807b71171503f20c651f17d3e0a4d170ab
prathamsirari7-gif <prathamsirari7@gmail.com>
2026-04-01 13:57:59 +0530

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, HttpResponse
+from home.models import Contact
+from django.contrib import messages
+from blog.models import Post
+# Create your views here.
+def home(request):
+    return render(request,'home/home.html')
+def about(request):
+    return render(request,'home/about.html')
+def contact(request): 
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        print(name,email,phone,content)
+
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<4:
+            messages.error(request,"Please fill the form correctly")
+        else:
+            contact=Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request,"Your messages has been sucessfully sent")
+    return render(request,'home/contact.html')
+def search(request):
+    query=request.GET['query']
+    if len(query)>78:
+        allPosts = Post.objects.none()
+    else:
+        allPostsTitle=Post.objects.filter(title__icontains=query)
+        allPostsContent=Post.objects.filter(content__icontains=query)
+        allPosts=allPostsTitle.union(allPostsContent)
+    if allPosts.count() == 0:
+        messages.error(request,"No Search results found.Please refine your query")
+    params={'allPosts':allPosts,'query':query}
+    return render(request,'home/search.html',params)
