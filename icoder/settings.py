9fe8a580512150b8eb312b523a3b349b584237df
prathamsirari7-gif <prathamsirari7@gmail.com>
2026-04-01 18:36:05 +0530

    fixed static final

--- icoder/settings.py
+++ icoder/settings.py
@@ -116,8 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 MESSAGE_TAGS = {
     messages.ERROR:'danger'
 }
